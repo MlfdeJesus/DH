ef07ff0732ac52b684f583821c3eaed02089345e
MlfdeJesus <MLFDEJESUS@GMAIL.COM>
2021-10-26 17:53:05 -0300

    Atividades e material da aula (25/10/2021)

--- DH/2º_bi/PROGRAMAÇÃO_IMPERATIVA/node/aula04/ex3.js
+++ DH/2º_bi/PROGRAMAÇÃO_IMPERATIVA/node/aula04/ex3.js
@@ -11,7 +11,6 @@ console.log(soma);
 
  w = 1 + "1";
  console.log(w);
- 
  w = "olá" + 23;
  console.log(w);
 
@@ -20,4 +19,50 @@ console.log(soma);
 
  console.log(nome+" "+sobrenome);
 
- 
+//exercicio 02
+
+
+
+//paciente 01
+let Nome = "Jose da Silva";
+let Idade = 27;
+let Peso = 83.5;
+let Altura = 1.70;
+let Plano = "Ouro"
+let IMC = Peso/(Altura**2);
+
+console.log(Nome+" tem "+Idade+" anos e seu índice de massa corporal é de "+IMC);
+
+
+//paciente 02
+let Nome2 = "Carlos de Souza";
+let Idade2 = 28;
+let Peso2 = 80.1;
+let Altura2 = 1.76;
+let Plano2 = "Diamante"
+let IMC2 = Peso2/(Altura2**2);
+
+console.log(Nome2+" tem "+Idade2+" anos e seu índice de massa corporal é de "+IMC2);
+
+
+
+
+//paciente 03
+let Nome3 = "Aline Ferreira";
+let Idade3 = 33;
+let Peso3 = 63.7;
+let Altura3 = 1.53;
+let Plano3 = "Não possui"
+let IMC3 = Peso3/(Altura3**2);
+
+console.log("Infelizmente podemos te informar o resultado do IMC, pois você não é assinante de nossos planos");
+
+//paciente 04
+let Nome4 = "Ana Paula";
+let Idade4 = 26;
+let Peso4 = 55;
+let Altura4 = 1.62;
+let Plano4 = "Prata"
+let IMC4 = Peso4/(Altura4**2);
+
+console.log(Nome4+" tem "+Idade4+" anos e seu índice de massa corporal é de "+IMC4);
