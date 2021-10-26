d0cfafc935c88da6db3bc2e9d2955075f4ddaf33
MlfdeJesus <MLFDEJESUS@GMAIL.COM>
2021-10-25 21:25:42 -0300

    Curso até 24/10/2021

---
+++ DH/2º_bi/PROGRAMAÇÃO_IMPERATIVA/node/aula04/ex3.js
@@ -0,0 +1,23 @@
+console.log(12+25);
+console.log(25-5);
+let x = 10;
+let y = 12;
+
+let soma = x+y;
+console.log(soma);
+
+ let w = 20+soma;
+ console.log(w);
+
+ w = 1 + "1";
+ console.log(w);
+ 
+ w = "olá" + 23;
+ console.log(w);
+
+ let nome = "Marcos";
+ let sobrenome = "Fonseca";
+
+ console.log(nome+" "+sobrenome);
+
+ 
